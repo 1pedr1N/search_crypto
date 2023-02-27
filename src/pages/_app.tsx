6ce3d770a83cb253b1e5d760c99f68e8bbd3095b
Pedro Henrique <pedrinfnn@gmail.com>
2023-02-27 20:14:50 -0300

    fix bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from '@/styles/globals'
 export default function App({ Component, pageProps }: AppProps) {
