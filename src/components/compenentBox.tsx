a8323483d3da959818ff216c759971589f46ccd7
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-03-01 21:23:48 -0300

    add component box

--- src/components/compenentBox.tsx
+++ src/components/compenentBox.tsx
@@ -3,7 +3,9 @@ import * as  S from '../styles/componentBox';
 const Box = () => {
     return (
         <S.Container>
-        <h1>Box</h1>
+        <h1>FOTO DA MOEDA</h1>
+        <S.Text>BITCOIN</S.Text>
+        <S.Value>US$ 50.000,00</S.Value>
         </S.Container>
     );
     };
--- src/styles/componentBox.ts
+++ src/styles/componentBox.ts
@@ -2,10 +2,21 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: row;
+    align-items: center;
     justify-content: space-between;
     border-radius: 20px;
     border:1px solid #000;
     height:150px;
     padding: 1rem;
     width: 30%;
+    `
+    export const Text = styled.p`
+    font-size: 1.5rem;
+   
+    color: #000;
+
+    `
+    export const Value = styled.p`
+    color:#3cb043;
+    font-size: 1.5rem;
     `
